579f052fe6088a3921712b3bced98aa399032523
hongyu <971976839@qq.com>
2017-03-23 16:49:32 +0800

    提交

--- application/build.php
+++ application/build.php
@@ -21,5 +21,13 @@ return [
         'model'      => ['User', 'UserType'],
         'view'       => ['index/index'],
     ],
+    // 定义index模块的自动生成 （按照实际定义的文件名生成）
+    'index'     => [
+        '__file__'   => ['common.php'],
+        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
+        'controller' => ['Index', 'Test', 'UserType'],
+        'model'      => ['User', 'UserType'],
+        'view'       => ['index/index'],
+    ],
     // 其他更多的模块定义
 ];
--- build.php
+++ build.php
@@ -13,7 +13,7 @@ return [
     // 生成应用公共文件
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
-    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
+    // 定义admin模块的自动生成 （按照实际定义的文件名生成）
     'admin'     => [
         '__file__'   => ['common.php'],
         '__dir__'    => ['behavior', 'controller', 'model', 'view'],
@@ -21,5 +21,6 @@ return [
         'model'      => ['User', 'UserType'],
         'view'       => ['index/index'],
     ],
+
     // 其他更多的模块定义
 ];
