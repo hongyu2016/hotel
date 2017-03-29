473d3250857a81e4397e80a367653d969639fc88
hongyu <971976839@qq.com>
2017-03-29 15:46:17 +0800

    后台静态页面模板--{block}  来继承模板 布局

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -6,7 +6,6 @@ use think\Request;
 class Login extends Controller
 {
     public function index(){
-
         //return view();
         return $this->fetch();
     }
@@ -14,14 +13,19 @@ class Login extends Controller
         $name=input('request.user-name');
         $password=input('request.password');
         $captcha=input('request.captcha');
-        if(!captcha_check($captcha)){
+
+        if(!captcha_check($captcha)){  //captcha_check 为验证验证码的
             //验证码验证失败
             return $this->error('验证码错误');
-
         };
+        $check=\app\admin\model\Admin::login($name,$password);  //引用Admin model
+        if($check){
+            $this->redirect("Index/index");
+            exit();
+        }else{
+            return $this->error('用户名或密码错误');
+        }
 
-        $this->redirect("Index/index");
-        exit();
     }
 
 }
---
+++ application/admin/model/Admin.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\admin\model;
+
+use think\Model;
+//文件命名对应着admin表
+class Admin extends Model
+{
+    public static function login($name,$password){
+        $where['admin_name']=$name;
+        $where['admin_password']=md5($password);//md5加密与数据库中的对比
+        $user=Admin::where($where)->find();
+        if ($user){
+            unset($user['password']);
+            session('ext_user',$user);
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
